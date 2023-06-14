6bafa2835379fdf0d173a676c26c186154c8b92a
mallikaraju <bnraju3070@gmail.com>
2023-06-14 08:21:19 +0530

    my firts commit

---
+++ src/main/java/com/bnr/bnrajuit/BnrajuitApplication.java
@@ -0,0 +1,13 @@
+package com.bnr.bnrajuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnrajuitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnrajuitApplication.class, args);
+	}
+
+}
